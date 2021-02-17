768ee1100721bc6137132285011655d3828c0621
mohdsherif <molpsychistb@gmail.com>
2021-02-17 14:46:26 -0500

    changed execfile to a python3 equivalent - removed the execfile function code I added

--- runone.py
+++ runone.py
@@ -30,26 +30,13 @@ if __name__ == "__main__":
     # h('proc setMemb() {}')
     h.xopen("init.hoc")
 
-
-    # to be able to use execfile (used to be in python2)
-    def execfile(filepath, globals=None, locals=None):
-        '''copied from https://stackoverflow.com/questions/436198/what-is-an-alternative-to-execfile-in-python-3'''
-        if globals is None:
-            globals = {}
-        globals.update({
-            "__file__": filepath,
-            "__name__": "__main__",
-        })
-        with open(filepath, 'rb') as file:
-            exec(compile(file.read(), filepath, 'exec'), globals, locals)
-
     from pyinit import *
     # from geom import *
     # from network import *
-    execfile("geom.py")
-    execfile("network.py")
-    execfile("params.py") # from params import *
-    execfile("run.py") # from run import *
+    exec(open("./geom.py").read()) # execfile("geom.py")
+    exec(open("./network.py").read()) # execfile("network.py")
+    exec(open("./params.py").read()) # execfile("params.py") # from params import *
+    exec(open("./run.py").read()) # execfile("run.py") # from run import *
 
     if dconf['recordNetStimInputs']:
         net.record_all_netStim_times()
